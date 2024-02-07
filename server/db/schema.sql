c4601777aa3b735ff2994851fcde1c71a30fb5ea
Andrej Durilkin <andrejd2021@yahoo.com>
2024-02-07 11:25:08 +0100

    Tabellen

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,3 +1,19 @@
-create TABLE test(
-    id int
+CREATE TABLE benutzer (
+	benutzerID INT NOT NULL AUTO_INCREMENT,
+	vorname VARCHAR(50),
+	nachname VARCHAR(50),
+	benutzername VARCHAR(50),
+	email VARCHAR(100),
+	passwort VARCHAR(50),
+	PRIMARY KEY(benutzerID)
+);
+
+CREATE TABLE Reservierung(
+	rID INT NOT NULL AUTO_INCREMENT,
+	bucher INT,
+	datum DATE,
+	zeitraum TIME,
+	anzahlPCs INT,
+	PRIMARY KEY(rID),
+	FOREIGN KEY(bucher) REFERENCES benutzer(benutzerID)
 );
