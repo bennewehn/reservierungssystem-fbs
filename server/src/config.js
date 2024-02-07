34281a82c196fd4eff3557afad635c7c01dc4273
Benedikt Wehner <benedikt.wehner@outlook.de>
2024-02-07 09:24:06 +0100

    add mysql dep

---
+++ server/src/config.js
@@ -0,0 +1,10 @@
+export const config = {
+  db: {
+    host: "localhost",
+    user: "root",
+    password: "test123",
+    database: "reservation",
+    connectTimeout: 60000
+  },
+  listPerPage: 10,
+};
--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express(); 
 const PORT = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res)=>{ 
     res.status(200); 
     res.send("Welcome to root URL of Server"); 
